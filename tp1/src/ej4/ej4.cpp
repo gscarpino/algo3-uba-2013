6ab20b84e9e2dbca4cc3d2e92060eae3da76dbc8
gino.scarpino@gmail.com <gino.scarpino@gmail.com@ec6796c5-0695-4c7b-b1e2-41967a99bd09>
2013-04-01 23:19:32 +0000

    tp1 ej4 solo lee entrada y carga datos

---
+++ tp1/src/ej4/ej4.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#define NEGRO 0;
+#define BLANCO 1;
+
+using namespace std;
+
+typedef vector<unsigned int> Vec;
+typedef vector<Vec> Matriz;
+
+void imprimirMatriz(const Matriz &m){
+    for(int i = 0; i < m.size(); i++){
+        for(int j = 0; j < m[0].size(); j++){
+            if(m[i][j]){
+                cout << " BLANCO ";
+            }
+            else{
+                cout << " NEGRO  ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if(argc <= 1) {
+      cout << "Modo de uso: ej1 archivoEntrada archivoSalida";
+      return 0;
+    }
+
+    ifstream inputFile(argv[1]);
+    if(!inputFile.is_open()){
+        cerr << "Error al abrir el archivo de entrada." << endl;
+    }
+
+    ofstream outputFile(argv[2],  ios_base::trunc);
+    if(!outputFile.is_open()){
+        cerr << "Error al abrir/crear el archivo de salida." << endl;
+    }
+
+    string linea;
+
+    while(!inputFile.eof()){
+        getline(inputFile, linea);
+        if(linea == "#"){
+            break;
+        }
+
+        istringstream sLinea(linea);
+        int n;
+        int temp = 0;
+        int dimN,dimM,cantPiezas;
+        while(!sLinea.eof()){
+            sLinea >> n;
+            if(temp == 0){
+                dimN = n;
+            }
+            else if(temp == 1){
+                dimM = n;
+            }
+            else if(temp == 2){
+                cantPiezas = n;
+            }
+            else{
+                cout << "ERROR LEYENDO ENTRADA" << endl;
+                return 1;
+            }
+            temp++;
+        }
+        Vec fila(dimM);
+        Matriz tablero;
+        for(int i = 0; i < dimN; i++){
+            getline(inputFile, linea);
+            istringstream sLinea2(linea);
+            for(int j = 0; j < dimM; j++){
+                sLinea2 >> n;
+                fila[j] = n;
+            }
+            tablero.push_back(fila);
+        }
+
+        vector<Matriz> piezas;
+
+        for(int i = 0; i < cantPiezas; i++){
+            getline(inputFile, linea);
+            istringstream sLinea2(linea);
+            int piezaN,piezaM;
+            sLinea2 >> piezaN;
+            sLinea2 >> piezaM;
+            Matriz pieza;
+            Vec fila2(piezaM);
+            for(int j = 0; j < piezaN; j++){
+                getline(inputFile, linea);
+                istringstream sLinea3(linea);
+                for(int k = 0; k < piezaM; k++){
+                    sLinea3 >> n;
+                    fila2[k] = n;
+                }
+                pieza.push_back(fila2);
+
+            }
+            piezas.push_back(pieza);
+        }
+
+        cout << "N: " << dimN << " - M: " << dimM << " - Piezas: " << cantPiezas << endl << endl;
+        cout << "Tablero: " << endl << endl;;
+        imprimirMatriz(tablero);
+        cout << endl << endl << "Piezas: " << endl;
+        for(int i = 0; i < cantPiezas; i++){
+            cout << endl << "- - -" << endl;
+            imprimirMatriz(piezas[i]);
+            cout << "- - -" << endl;
+        }
+
+//        resolver(tablero,dimN,dimM,piezas);
+    }
+
+    inputFile.close();
+    outputFile.close();
+    return 0;
+}
