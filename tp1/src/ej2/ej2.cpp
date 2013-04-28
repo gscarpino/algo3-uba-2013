7f63a7ea53ee6aafa79b4784aaeee86f07f62403
gino.scarpino <gino.scarpino@ec6796c5-0695-4c7b-b1e2-41967a99bd09>
2013-04-28 03:26:27 +0000

    Resultados ej2

--- tp1/src/ej2/ej2.cpp
+++ tp1/src/ej2/ej2.cpp
@@ -9,7 +9,7 @@
 #include <algorithm>
 #include <time.h>
 
-#define TESTING_AZAR 1
+#define TESTING_AZAR 0
 #define RESULTADOS 1
 
 using namespace std;
@@ -35,7 +35,7 @@ timespec diff(timespec start, timespec end)
 
 void  mostrarVecInt(const vector<int> &x){
     cout << "[ ";
-    for(int i = 0; i < x.size(); i++){
+    for(unsigned int i = 0; i < x.size(); i++){
 //        cout << "(" << x[i].first << "," << x[i].second << ") ";
         cout << x[i] << " ";
     }
@@ -43,7 +43,7 @@ void  mostrarVecInt(const vector<int> &x){
 }
 void  mostrarVecIntTupla(const vector<pair<int,int> > &x){
     cout << "[ ";
-    for(int i = 0; i < x.size(); i++){
+    for(unsigned int i = 0; i < x.size(); i++){
 //        cout << "(" << x[i].first << "," << x[i].second << ") ";
         cout << "(" << x[i].first << "," << x[i].second << ") ";
     }
@@ -82,18 +82,22 @@ int main(int argc, char *argv[]) {
     cout << "Ejercicio 2 - Sensores defectuosos" << endl << endl;
 
     if(TESTING_AZAR){
-        cout << "Creando tests con k fijo..." << endl;
+        cout << "Creando tests con K fijo..." << endl;
         int maxSensores = 100;
-        int repeticiones = 100;
-        int minSensores = 10;
+        int repeticiones = 1000;
+        int minSensores = 1;
         int tiempo = 0;
         int falla = 0;
+        int maxfalla;
+        vector<int> sens(5);
+
+
         ofstream archTesting("testingAzarKfijo.txt");
         if(archTesting.is_open()){
             srand(time(NULL));
-            for(int s = minSensores; s < maxSensores;s++){
+            for(int s = minSensores; s <= maxSensores;s++){
                 for(int r = 0; r < repeticiones; r++){
-                    falla = s * 20 + rand() % s + 1;
+                    falla = s * 4 + rand() % s + 1;
                     archTesting << s << " " << falla;
                     for(int i = 0; i < s; i++){
                         tiempo = 1 + rand() % 20;
@@ -102,78 +106,75 @@ int main(int argc, char *argv[]) {
                     archTesting << "\n";
                 }
             }
+            archTesting << "#";
         }
-        archTesting << "#";
         archTesting.close();
+
 
         cout << "Tests creados." << endl;
 
-        cout << "Creando tests con n fijo..." << endl;
-        int maxfalla;
-        archTesting.open("testingAzarNfijo.txt");
+        cout << "Creando tests con N fijo..." << endl;
+        repeticiones = 100;
+        archTesting.open("testingAzarNfijo_5.txt");
         if(archTesting.is_open()){
             srand(time(NULL));
             //cant sensores baja:
-            vector<int> sens(10);
-            for(int i = 0; i < sens.size(); i++){
-                sens[i] = 1 + rand() % 20;
+            for(unsigned int i = 0; i < sens.size(); i++){
+                sens[i] = 1 + rand() % 10;
             }
             maxfalla = sens.size() * 50;
-            for(int falla = 1; falla < maxfalla;falla++){
+            for(int falla = 1; falla <= maxfalla;falla++){
                 for(int r = 0; r < repeticiones; r++){
                     archTesting << sens.size() << " " << falla;
-                    for(int i = 0; i < sens.size(); i++){
+                    for(unsigned int i = 0; i < sens.size(); i++){
                         archTesting << " " << sens[i];
                     }
                     archTesting << "\n";
                 }
             }
-
-            sens.resize(100);
-            for(int i = 0; i < sens.size(); i++){
-                sens[i] = 1 + rand() % 20;
-            }
-            maxfalla = sens.size() * 50;
-            for(int falla = 1; falla < maxfalla;falla++){
-                for(int r = 0; r < repeticiones; r++){
-                    archTesting << sens.size() << " " << falla;
-                    for(int i = 0; i < sens.size(); i++){
-                        archTesting << " " << sens[i];
-                    }
-                    archTesting << "\n";
-                }
-            }
-
-
-            sens.resize(1000);
-            for(int i = 0; i < sens.size(); i++){
-                sens[i] = 1 + rand() % 20;
-            }
-            maxfalla = sens.size() * 50;
-            for(int falla = 1; falla < maxfalla;falla++){
-                for(int r = 0; r < repeticiones; r++){
-                    archTesting << sens.size() << " " << falla;
-                    for(int i = 0; i < sens.size(); i++){
-                        archTesting << " " << sens[i];
-                    }
-                    archTesting << "\n";
-                }
-            }
+            archTesting << "#";
         }
-        archTesting << "#";
         archTesting.close();
+
+
+        archTesting.open("testingAzarNfijo_50.txt");
+        if(archTesting.is_open()){
+            sens.resize(50);
+            for(unsigned int i = 0; i < sens.size(); i++){
+                sens[i] = 1 + rand() % 20;
+            }
+            maxfalla = sens.size() * 50;
+            for(int falla = 1; falla <= maxfalla;falla++){
+                for(int r = 0; r < repeticiones; r++){
+                    archTesting << sens.size() << " " << falla;
+                    for(unsigned int i = 0; i < sens.size(); i++){
+                        archTesting << " " << sens[i];
+                    }
+                    archTesting << "\n";
+                }
+            }
+            archTesting << "#";
+        }
+        archTesting.close();
+
 
         cout << "Tests creados." << endl;
         return 0;
     }
 
 
+/*//////////////////////////////////////////////////////////////////////*/
+
+//    argv[1] = "testingAzarKfijo.txt";
+//    argv[2] = "ResultadoAzarKfijo.txt";
+
 
     if(argc <= 1) {
       cout << "Modo de uso: ej2 archivoEntrada archivoSalida";
       return 1;
     }
-    argv[1] = "testingAzarNfijo.txt";
+
+
     ifstream arch(argv[1]);
     if(!arch.is_open()){
         cerr << "Error al abrir el archivo de entrada." << endl;
@@ -186,9 +187,11 @@ int main(int argc, char *argv[]) {
 
     string linea;
     if(RESULTADOS){
-        archSalida.close();
-        archSalida.open("resultados.csv");
-        archSalida << "Sensores;Falla;Sensor;Tiempo;" << endl;
+//        archSalida.close();
+//        archSalida.open("resultados.csv");
+//        archSalida << "Sensores;Falla;Sensor;Tiempo;" << endl;
+//        archSalida.open("resultados.txt");
+        archSalida << "#Sensores Falla Sensor Tiempo" << endl;
     }
 
     unsigned int corrida = 1;
@@ -230,7 +233,11 @@ int main(int argc, char *argv[]) {
         timespec comienzo;
         timespec terminacion;
         clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &comienzo);
-        if(medDefectuosa >= sensores.size()){
+
+        if(sensores.size() == 1){
+            medDefectuosa = 1;
+        }
+        else if(medDefectuosa > sensores.size()){
 
 
             make_heap(tiemposHeap.begin(),tiemposHeap.end(),cmpHeap);   //O(3*n)
@@ -256,7 +263,9 @@ int main(int argc, char *argv[]) {
 //        cout << endl << "Sensor que fallo en la medicion " << medDefectuosa << ": " << medicionesHeap[medDefectuosa-1] << endl;
         cout << "Corrida " << corrida << endl;
         if(RESULTADOS){
-            archSalida << cant << ";" << medDefectuosa << ";" << medicionesHeap[medDefectuosa-1] << ";" << diff(comienzo,terminacion).tv_nsec << ";" << endl;
+            if(diff(comienzo,terminacion).tv_nsec > 0)
+//            archSalida << cant << ";" << medDefectuosa << ";" << medicionesHeap[medDefectuosa-1] << ";" << diff(comienzo,terminacion).tv_nsec << ";" << endl;
+            archSalida << cant << " " << medDefectuosa << " " << medicionesHeap[medDefectuosa - 1] << " " << diff(comienzo,terminacion).tv_nsec << endl;
         }
         else{
             archSalida << medicionesHeap[medDefectuosa-1] << endl;
