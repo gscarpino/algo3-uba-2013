4b94e01b3ca977a1d517617608a969b612cbdcf7
gino.scarpino@gmail.com <gino.scarpino@gmail.com@ec6796c5-0695-4c7b-b1e2-41967a99bd09>
2013-05-06 19:09:50 +0000

    Creo que esta terminado

--- tp2/src/ej2/main.cpp
+++ tp2/src/ej2/main.cpp
@@ -16,6 +16,7 @@
 using namespace std;
 
 void genTests();
+bool estaEn(const vector< Camino > &caminos, const Camino &c);
 
 timespec diff(timespec start, timespec end){
         timespec temp;
@@ -41,6 +42,8 @@ int main(int argc, char * argv[]){
         exit(1);
     }
 
+//    argv[1] = "testsPocasRutasExistentes.txt";
+
     ifstream inputFile(argv[1]);
     if(!inputFile.is_open()){
         cerr << "Error al abrir el archivo de entrada." << endl;
@@ -57,7 +60,7 @@ int main(int argc, char * argv[]){
     ofstream archRes;
     if(RESULTADOS){
         archRes.open("resultado.csv");
-        archRes << "Nodos AristasExistentes Tiempo";
+        archRes << "Nodos AristasExistentes Tiempo" << endl;
     }
 
     string linea;
@@ -69,7 +72,7 @@ int main(int argc, char * argv[]){
         unsigned int cantCiudades = atoi(linea.c_str());
 
         Grafo ciudades(cantCiudades);
-
+        unsigned int contRutasExistentes = 0;
         for(unsigned int i = 0; i < ((cantCiudades * (cantCiudades - 1))/2); i++){
             unsigned int c1,c2,e,p;
             getline(inputFile, linea);
@@ -77,6 +80,7 @@ int main(int argc, char * argv[]){
             sLinea >> c1;
             sLinea >> c2;
             sLinea >> e;
+            if(e) contRutasExistentes++;
             sLinea >> p;
             Camino c(c1,c2,e,p);
             ciudades.agregarArista(c);
@@ -87,12 +91,44 @@ int main(int argc, char * argv[]){
         if(RESULTADOS){
             clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &comienzo);
         }
-        Grafo ciudadesModificadas = ciudades.AGM();
+        Grafo ciudadesModificadas(ciudades.AGM());
         if(RESULTADOS){
             clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &terminacion);
-            archRes << cantCiudades << " " << ciudades.getExistentes() << " " << diff(comienzo,terminacion).tv_nsec << endl;
+            archRes << cantCiudades << " " << contRutasExistentes << " " << diff(comienzo,terminacion).tv_nsec << endl;
         }
-        cout << "c: " << cantCiudades << endl;
+
+        if(ciudadesModificadas.cantAristas() == 0){
+            cout << "Error" << endl;
+            archRes.close();
+            inputFile.close();
+            outputFile.close();
+            exit(1);
+        }
+
+        unsigned int costoTotal = 0;
+        vector< Camino > caminos(ciudades.getAristas());
+        for(unsigned int i = 0; i < caminos.size(); i++){
+            caminos[i].imprimir();
+            if(caminos[i].getExiste()){
+                if(!estaEn(ciudadesModificadas.getAristas(),caminos[i])){
+                    costoTotal = costoTotal + caminos[i].getCosto();
+                }
+            }
+            else{
+                if(estaEn(ciudadesModificadas.getAristas(),caminos[i])){
+                    costoTotal = costoTotal + caminos[i].getCosto();
+                }
+            }
+        }
+
+        outputFile << costoTotal << " " << ciudadesModificadas.cantAristas();
+        caminos = ciudadesModificadas.getAristas();
+        cout << endl;
+        for(unsigned int i = 0; i < caminos.size(); i++){
+            caminos[i].imprimir();
+            outputFile << " " << caminos[i].getCiudad1() << " " << caminos[i].getCiudad2();
+        }
+
 
     }
     archRes.close();
@@ -101,8 +137,27 @@ int main(int argc, char * argv[]){
     return 0;
 }
 
+bool estaEn(const vector< Camino > &caminos, const Camino &c){
+    bool res = false;
+
+    for(unsigned int i = 0; i < caminos.size();i++){
+        if(caminos[i].getCiudad1() == c.getCiudad1() && caminos[i].getCiudad2() == c.getCiudad2()){
+            res = true;
+            break;
+        }
+        if(caminos[i].getCiudad1() == c.getCiudad2() && caminos[i].getCiudad2() == c.getCiudad1()){
+            res = true;
+            break;
+        }
+    }
+
+    return res;
+}
+
 void genTests(){
-    ofstream outputFile("testsBajaRutasExistentes.txt",  ios_base::trunc);
+
+    cout << "Creando tests pocas rutas existentes." << endl;
+    ofstream outputFile("testsPocasRutasExistentes.txt",  ios_base::trunc);
     if(!outputFile.is_open()){
         cerr << "Error al abrir/crear el archivo de salida." << endl;
         exit(1);
@@ -111,7 +166,7 @@ void genTests(){
     unsigned int maxNodos = 250;
     unsigned int repeticiones = 50;
     unsigned int costo;
-    bool existe;
+//    bool existe;
     for(unsigned int i = 4; i <= maxNodos; i++){
         for(unsigned int r = 0; r < repeticiones; r++){
             outputFile << i << endl;
@@ -119,25 +174,55 @@ void genTests(){
                 for(unsigned int b = a+1; b < i; b++){
                     costo = rand() % 10000 + 500;
                     if(a==0 && b==1){
-                        outputFile << a << " " << b << " " << 1 << costo << endl;
+                        outputFile << a << " " << b << " " << 1 << " " << costo << endl;
                     }
                     else{
                         if(rand()%100 < 20){
-                            outputFile << a << " " << b << " " << 1 << costo << endl;
+                            outputFile << a << " " << b << " " << 1 << " " << costo << endl;
                         }
                         else{
-                            outputFile << a << " " << b << " " << 0 << costo << endl;
+                            outputFile << a << " " << b << " " << 0 << " " << costo << endl;
                         }
                     }
                 }
             }
         }
-        cout << "Ciudades: " << i << endl;
     }
 
     outputFile << "#";
     outputFile.close();
+    cout << "Tests creados." << endl << endl;
 
-//    outputFile.open("testsBajaAltaExistentes.txt");
 
+    cout << "Creando tests muchas rutas existentes." << endl;
+    outputFile.open("testsMuchasRutasExistentes.txt");
+    if(!outputFile.is_open()){
+        cerr << "Error al abrir/crear el archivo de salida." << endl;
+        exit(1);
+    }
+    for(unsigned int i = 4; i <= maxNodos; i++){
+        for(unsigned int r = 0; r < repeticiones; r++){
+            outputFile << i << endl;
+            for(unsigned int a = 0; a < i; a++){
+                for(unsigned int b = a+1; b < i; b++){
+                    costo = rand() % 10000 + 500;
+                    if(a==0 && b==1){
+                        outputFile << a << " " << b << " " << 1 << " " << costo << endl;
+                    }
+                    else{
+                        if(rand()%100 < 70){
+                            outputFile << a << " " << b << " " << 1 << " " << costo << endl;
+                        }
+                        else{
+                            outputFile << a << " " << b << " " << 0 << " " << costo << endl;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    outputFile << "#";
+    outputFile.close();
+    cout << "Tests creados." << endl << endl;
 }
