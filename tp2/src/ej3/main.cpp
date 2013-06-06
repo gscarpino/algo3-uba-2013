a11069e8f7e729345b4de9ee311f37e9fcef4213
gino.scarpino@gmail.com <gino.scarpino@gmail.com@ec6796c5-0695-4c7b-b1e2-41967a99bd09>
2013-06-06 18:01:00 +0000

    testing del ej3 falto comitear el codigo final

--- tp2/src/ej3/main.cpp
+++ tp2/src/ej3/main.cpp
@@ -8,9 +8,21 @@
 #include "GrafoEj3.h"
 
 #define TESTING 0
-#define RESULTADOS 0
+#define RESULTADOS 1
 
 void genTests();
+
+timespec diff(timespec start, timespec end){
+        timespec temp;
+        if ((end.tv_nsec-start.tv_nsec)<0) {
+                temp.tv_sec = end.tv_sec-start.tv_sec-1;
+                temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+        } else {
+                temp.tv_sec = end.tv_sec-start.tv_sec;
+                temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+        }
+        return temp;
+}
 
 using namespace std;
 
