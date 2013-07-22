0882d4d645bda14c0c87fca078d15ea64967e924
raulbenitti@gmail.com <raulbenitti@gmail.com@ec6796c5-0695-4c7b-b1e2-41967a99bd09>
2013-07-22 02:17:09 +0000

    src - entregable

--- tp3/src - entregable/grasp.cpp
+++ tp3/src - entregable/grasp.cpp
@@ -2,8 +2,7 @@
 #include "goloso.h"
 #include "local.h"
 
-vector<unsigned int> maximoImpactoGrasp(const Grafo &G, const Grafo &H, double porcentaje)
-{
+vector<unsigned int> maximoImpactoGrasp(const Grafo &G, const Grafo &H, double porcentaje) {
     vector<unsigned int> res(G.cantNodos() + 1);
     res[0] = 0;
 
@@ -13,39 +12,30 @@ vector<unsigned int> maximoImpactoGrasp(const Grafo &G, const Grafo &H, double p
     //Optimizados
     unsigned int maxIteraciones;
     unsigned int maxIterSinMejora;
-    if(G.cantNodos() < 50)
-    {
-        maxIteraciones = G.cantNodos() * 10;
-        maxIterSinMejora = maxIteraciones * 0.2;
-    }
-    else
-    {
+    if(G.cantNodos() <= 50) {
+        maxIteraciones = G.cantNodos() * 5;
+    } else {
         maxIteraciones = G.cantNodos() * 3;
-        maxIterSinMejora = maxIteraciones * 0.5;
     }
     //Optimizados
     unsigned int sinMejora = 0;
     unsigned int maxRCL = 5;
     unsigned int elegido;
+    maxIterSinMejora = maxIteraciones * 0.5;
 
     //Optimización del parámetro
-    if(G.cantNodos() < 75)
-    {
+    if(G.cantNodos() < 75) {
         porcentaje = 0.3;
-    }
-    else
-    {
+    } else {
         porcentaje = 0.1;
     }
 
 
 
-    for(unsigned int i = 0; i < maxIteraciones; i++)
-    {
+    for(unsigned int i = 0; i < maxIteraciones; i++) {
 
         vector< vector<unsigned int> > rcl(maxRCL);
-        for(unsigned int k = 0; k < maxRCL; k++)
-        {
+        for(unsigned int k = 0; k < maxRCL; k++) {
             rcl[k] = maximoImpactoGoloso(G,H,porcentaje);
         }
         elegido = rand()%maxRCL;
@@ -54,20 +44,15 @@ vector<unsigned int> maximoImpactoGrasp(const Grafo &G, const Grafo &H, double p
 
         solBusqLocal = maximoImpactoLocal(G,H,rcl[elegido]);
 
-        if(solBusqLocal[0] > res[0])
-        {
+        if(solBusqLocal[0] > res[0]) {
             res[0] = solBusqLocal[0];
-            for(unsigned int k = 1; k < solBusqLocal.size(); k++)
-            {
+            for(unsigned int k = 1; k < solBusqLocal.size(); k++) {
                 res[k] = solBusqLocal[k];
             }
             sinMejora = 0;
-        }
-        else
-        {
+        } else {
             sinMejora++;
-            if(sinMejora == maxIterSinMejora)
-            {
+            if(sinMejora == maxIterSinMejora) {
                 break;
             }
         }
